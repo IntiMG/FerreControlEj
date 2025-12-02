df9a4ef468920ef20232ebe668646eb1dd2dd861
IntiMG <164436501+IntiMG@users.noreply.github.com>
2025-12-02 17:54:21 -0600

    Add purchase detail to compra view

--- FerreControl/src/main/java/com/tuempresa/FerreControl/modelo/DetalleCompra.java
+++ FerreControl/src/main/java/com/tuempresa/FerreControl/modelo/DetalleCompra.java
@@ -7,6 +7,7 @@ import org.openxava.annotations.*;
 import lombok.*;
 
 @Entity @Getter @Setter
+@View(members="producto; cantidad; costoUnitario; subtotal")
 public class DetalleCompra {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Hidden
@@ -14,7 +15,7 @@ public class DetalleCompra {
 
     @ManyToOne(fetch = FetchType.LAZY) @Required
     @JoinColumn(name="COMPRA_idCompra", nullable=false)
-    private com.tuempresa.FerreControl.modelo.Compra compra; // Relación N:1 con Compra (FK)
+    private com.tuempresa.FerreControl.modelo.Compra compra; // RelaciÃ³n N:1 con Compra (FK)
 
     @ManyToOne(fetch = FetchType.LAZY) @Required
     @DescriptionsList(descriptionProperties="nombre")
